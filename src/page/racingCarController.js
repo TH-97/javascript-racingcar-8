50cb1cdd001a35d0c69d3a1ea79332bf4a131294
TH_97 <hood3392@gmail.com>
2025-10-27 14:27:29 +0900

    feat(controller) : controller 기능 구현

--- src/page/racingCarController.js
+++ src/page/racingCarController.js
@@ -1,5 +1,44 @@
-import { outputGuideMessage } from "../features/output/index.js";
+import { inputValues } from "../features/input/index.js";
+import {
+  outputGuideMessage,
+  outputAttemptMessage,
+  outputRaceResult,
+  outputResultMessage,
+  outputWinnerMessage,
+} from "../features/output/index.js";
+import {
+  valitdateCarName,
+  valitdateAttemptCount,
+} from "../features/validator/index.js";
+import { getRandomNumberArray } from "../features/getRandomNumberArray/index.js";
+import { Race } from "../entites/race/index.js";
+import { Console } from "@woowacourse/mission-utils";
 
 export default async function racingCarController() {
   outputGuideMessage();
+  const inputCarName = await inputValues();
+
+  await valitdateCarName(inputCarName);
+  await outputAttemptMessage();
+  const carNameArray = await splitInput(inputCarName);
+
+  const attemptCount = await inputValues();
+
+  await valitdateAttemptCount(attemptCount);
+
+  const race = new Race(carNameArray);
+  outputResultMessage();
+  for (let i = 0; i < attemptCount; i++) {
+    const randomNumberArray = getRandomNumberArray(carNameArray.length);
+    race.palyRound(randomNumberArray);
+    const stateArray = race.getState();
+    outputRaceResult(stateArray, carNameArray);
+    Console.print("");
+  }
+  const winnerArray = await race.getFinalWinner();
+  outputWinnerMessage(winnerArray);
+}
+
+function splitInput(input) {
+  return input.split(",");
 }
