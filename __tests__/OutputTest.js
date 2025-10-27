ef0df45e3a062a5d5587a57e7c5844f928db6d68
TH_97 <hood3392@gmail.com>
2025-10-27 14:50:36 +0900

    fix(output) : 테스트 케이스 추가

--- __tests__/OutputTest.js
+++ __tests__/OutputTest.js
@@ -86,4 +86,23 @@ describe("output 테스트", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
     });
   });
+
+  test("동률 테스트2", async () => {
+    // given
+    const inputs = ["pobi,woni", "1"];
+    const logs = ["pobi : ", "woni : ", "최종 우승자 : pobi, woni"];
+    const logSpy = getLogSpy();
+
+    mockQuestions(inputs);
+    mockRandoms([3, 3]);
+
+    // when
+    const app = new App();
+    await app.run();
+
+    // then
+    logs.forEach((log) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+    });
+  });
 });
