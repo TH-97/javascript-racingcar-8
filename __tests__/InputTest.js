95526574352985e4df2d09cb178b703d3dc5eeb3
TH_97 <hood3392@gmail.com>
2025-10-27 14:10:18 +0900

    feat(Test) : Input 테스트 케이스 추가

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -10,7 +10,29 @@ const mockQuestions = (inputs) => {
   });
 };
 describe("input 테스트", () => {
-  test("자동차 이름 입력 예외 테스트", async () => {
+  test("자동차 이름 입력 공백 예외 테스트", async () => {
+    // given
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  test("자동차 이름 입력 중복 예외 테스트", async () => {
+    // given
+    const inputs = ["asd,asd"];
+    mockQuestions(inputs);
+
+    // when
+    const app = new App();
+
+    // then
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  test("자동차 이름 입력 5자 초과시 예외 테스트", async () => {
     // given
     const inputs = ["pobi,javaji"];
     mockQuestions(inputs);
